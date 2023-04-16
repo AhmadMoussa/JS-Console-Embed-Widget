677643cbb4391088a990ae8e03c349f688332ddb
Ahmad Moussa <gorillasuncontact@gmail.com>
2023-04-16 14:46:15 +0200

    Update simple.js

--- src/simple.js
+++ src/simple.js
@@ -2,15 +2,13 @@
     const Widget = Object.create({
         create(id) {
             const wdg = document.createElement("div");
-            wdg.classList.add("chat-box");
-            wdg.innerHTML = `<header><style>.logFrame-${ id },.runButton-${ id },.textArea-${ id }{border:1px solid #999;margin:2px;padding:3px}.widgetContainer-${ id }{display:flex;flex-direction:row;justify-content:space-between}.buttonAndInput-${ id }{display:flex;flex-direction:column}.logFrame-${ id }{flex:1 0}.textArea-${ id }{flex:1 0;resize:horizontal}</style></header><div class=\"widgetContainer-${ id }\"><div class=\'buttonAndInput-${ id }\'><textarea id=\'code-${ id }\' class=\'textArea-${ id }\'></textarea><button id=\'safe-${ id }\' class=\'runButton-${ id }\'>Run</button></div><iframe sandbox=\'allow-scripts\' id=\'sandboxed-${ id }\' class=\'logFrame-${ id }\'></iframe></div>`;
+            wdg.innerHTML = `<header><style>.logFrame-${ id },.runButton-${ id },.textArea-${ id }{border:1px solid #999;margin:2px;padding:3px}.widgetContainer-${ id }{display:flex;flex-direction:row;justify-content:space-between}.buttonAndInput-${ id }{display:flex;flex-direction:column}.logFrame-${ id }{flex:1 0}.textArea-${ id }{flex:1 0;resize:horizontal}</style></header><div class=\"widgetContainer-${ id }\"><div class=\'buttonAndInput-${ id }\'><textarea id=\'code-${ id }\' class=\'textArea-${ id }\'></textarea><label for=\'code-${ id }\' style=\'font-size:0pt\'>Editor Pane</label><button id=\'safe-${ id }\' class=\'runButton-${ id }\' aria-label="Run">Run</button></div><iframe sandbox=\'allow-scripts\' id=\'sandboxed-${ id }\' class=\'logFrame-${ id }\' title=\'Console Frame\'></iframe></div>`;
 
             console.log(wdg)
             wdg.script = document.createElement("script")
             wdg.script.setAttribute('type', 'text/javascript');
             wdg.script.appendChild(document.createTextNode(`function evaluate(){var e=document.getElementById(\"sandboxed-${ id }\"),t=document.getElementById(\"code-${ id }\").value;e.contentWindow.postMessage(t,\"*\")}window.addEventListener(\"message\",function(e){var t=document.getElementById(\"sandboxed-${ id }\");\"null\"===e.origin&&e.source===t.contentWindow&&alert(\"Result: \"+e.data)}),document.getElementById(\"safe-${ id }\").addEventListener(\"click\",evaluate);`))
             wdg.appendChild(wdg.script)
-            // Load your chat data into UI
             return wdg;
         }
     });
